22b6af5250c6cb02dd7c5558b73648ecc7170ccd
hahahahahzt <48832145+hahahahahzt@users.noreply.github.com>
2020-05-19 12:55:39 +0800

    Add files via upload

--- run.py
+++ run.py
@@ -1,4 +1,10 @@
 from pretreatment.topology import draw_topology_test
-
+from pretreatment.get_alarm import alarm_info
 
 draw_topology_test()
+ainfo = alarm_info()
+# 更新训练集
+ainfo.add_feature()
+# 对告警信息进行分类和存储
+ainfo.get_alarm_type()
+ainfo.get_alarm_info()
